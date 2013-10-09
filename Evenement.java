5e32d358e11caa9404a9684ddd1daa8c644633b7
ceccaldi <thibaut.ceccaldi.sio@gmail.com>
2013-10-09 15:44:18 +0200

    commit #3 : constructeur

--- Evenement.java
+++ Evenement.java
@@ -1,5 +1,10 @@
-class Evenement{
+public class Evenement{
 	private String nom ; 
 	private String lieu ; 
+	
+	public Evenement(String nom, String lieu){
+		this.nom = nom ;
+		this.lieu = lieu ;
+	}
 	
 }
